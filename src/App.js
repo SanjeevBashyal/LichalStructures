af6df4488dc2782db450fcb8296af220ee1e4570
Sanjeev Bashyal <sanjeev.bashyal01@gmail.com>
2021-10-01 07:35:40 +0545

    Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
-// import HeaderClass from './Components/HeaderClass'
-import Footer from './Components/Footer/Footer'
+import React from 'react';
+import Navbar from './components/Navbar';
+// import './App.css';
+// import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import Services from './components/pages/Services';
+// import Products from './components/pages/Products';
+// import SignUp from './components/pages/SignUp';
+
 function App() {
   return (
-    <div className="App">
-      <Footer/>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+        <Route path='/' exact />
+          {/* <Route path='/' exact component={Home} /> */}
+          {/* <Route path='/services' component={Services} /> */}
+          {/* <Route path='/products' component={Products} /> */}
+          {/* <Route path='/sign-up' component={SignUp} /> */}
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
